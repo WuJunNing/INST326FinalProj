776782c8fb767a5c6d37cdc18fe3b53361e62799
Simon Wu <swu81301@terpmail.umd.edu>
2022-04-15 12:25:18 -0400

    Simon employee class update

---
+++ MainFIleTest.py
@@ -0,0 +1,53 @@
+from re import L
+import random
+
+class Store():
+    def __init__(self, start_rent, maxinv):
+        
+        self.profit = 0
+        self.money = 1000
+        self.inv = {}
+        #self.salary = salary
+        self.rent = start_rent
+        self.maxinv = maxinv
+    def buy_inventory():
+        pass
+    def set_prices():
+        pass
+    def manage_expenses():
+        pass
+class GameState():
+    def __init__(self, products):
+        self.day = 0
+        self.wholesale_stock = products
+        
+    def run_game(self):
+        pass
+    def run_day(self):
+        pass
+    
+    def simulate_day(self):
+        pass
+    def store_status(self):
+        pass
+
+#types of days
+def event():
+    events = []
+    dayType = random.randint(0, len(events))
+#employee class with subclasses for different positions
+class employee():
+    def __init__(salary, position):
+        self.salary = salary
+        self.position = position
+        
+class boss():
+class manager():
+class worker():
+    
+if __name__ == "__main__":
+    pass
+    
+
+        
+        
