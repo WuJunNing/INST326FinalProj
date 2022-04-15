ed38d7001f3f72b9d2919bfb52224565046acabe
Luis <luisgomez3017@gmail.com>
2022-04-15 11:46:16 -0400

    Testing mainFileTest Update

--- MainFIleTest.py
+++ MainFIleTest.py
@@ -1,13 +1,18 @@
-from selectors import EpollSelector
 
-
+placeHolderItems = {"Milk": 3.30, "Meat": 5.00, "Cereal":4.00}
+class employee():
+    def __init__(self, name, salary, schedule):
+        self.name = name
+        self.salary = salary
+        self.schedule = schedule
+        
 class Store():
-    def __init__(self, Funds, size, inv):
+    def __init__(self,name, Funds, size, inv):
+        self.name = name
         self.funds = Funds
         self.size = size
         self.inventory = inv
     def buyInventory(self, InvItem, amountPurchased):
-        placeHolderItems = {"Milk": 3.30, "Meat": 5.00, "Cereal":4.00}
         stockBuying = placeHolderItems.get(InvItem) * amountPurchased
         confirmPurchase = input(f"Your currently purchasing {amountPurchased} of {InvItem}, this will cost you {stockBuying}, which will reduce store funds to {self.funds - stockBuying} (Y/N)")
         if confirmPurchase.upper() == "Y":
@@ -15,4 +20,11 @@ class Store():
             return amountPurchased
         else:
             print("Purchased declined")
-            
+    def setPrices(self, InventorySelection, newPrice):
+        print("Current Store prices for inventory items")
+        [print("lineItem\n") for lineItem in placeHolderItems]
+        changeprice1 = input("Which item would you like to change to?")
+        NewPrice = placeHolderItems.get(changeprice1) 
+        placeHolderItems[f"{InventorySelection}"] = NewPrice
+        placeHoldCont = input(f"{InventorySelection} New Price Set: {newPrice}")
+    
